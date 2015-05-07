572fff7e5f69a50584269ac3b560cb6ada35aa9e
zhongxw <zhongxw@qssec.com>
2015-05-07 14:54:22 +0800

    UPDATE: AlreadyRegistered at /admin/

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,8 +5,8 @@ Created on 2015-05-05
 @author: xiaowei
 '''
 
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from django.contrib import admin
 
 
-admin.site.register(User)
+# admin.site.register(User)
