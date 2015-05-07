4e43d5e5943aaa10c6d1fbbddf31c657b198ed35
zhongxw <zhongxw@qssec.com>
2015-05-07 15:41:28 +0800

    FIX: 404

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,7 +9,7 @@ from django.conf.urls import patterns, include, url
 
 
 urlpatterns = patterns('',
-    url(r'^/login/$', 'user_login', name='login'),
-    url(r'^/logout/$', 'user_logout', name='logout'),
+    url(r'^login/$', 'user_login', name='login'),
+    url(r'^logout/$', 'user_logout', name='logout'),
     url(r'^/$', 'home', name='home'),
 )
