6b72481bdcb7631e46003821cb3a238420a3c3de
zhongxw <zhongxw@qssec.com>
2015-05-07 15:49:09 +0800

    FIX: 404

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,11 +5,10 @@ Created on 2015-05-05
 @author: xiaowei
 '''
 
-from accounts import views
 from django.conf.urls import patterns, include, url
 
 
-urlpatterns = patterns(views,
+urlpatterns = patterns('accounts.views',
     url(r'^login/$', 'user_login', name='login'),
     url(r'^logout/$', 'user_logout', name='logout'),
     url(r'^/$', 'home', name='home'),
