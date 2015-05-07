ee3d08dedbf07eedfea5882648cc7626a18cfb00
zhongxw <zhongxw@qssec.com>
2015-05-07 15:55:42 +0800

    FIX: 403

--- agentV2/settings.py
+++ agentV2/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 PROJECT_DIR = os.path.dirname(__file__)
-print PROJECT_DIR
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # import sys
