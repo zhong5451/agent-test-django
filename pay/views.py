632931f6d77d7cb7d0b0a10dcc572ccef443123e
zhongxw <zhongxw@qssec.com>
2015-05-11 17:32:53 +0800

    UPDATE: update return_code

--- pay/views.py
+++ pay/views.py
@@ -83,7 +83,7 @@ def pay_by_alipay(request):
     print request_url
     return_data = {}
     return_data['alipay_url'] = request_url
-    return_data.update()
+    return_data.update({'return_code': 1})
     return json_response(return_data)
 
 
