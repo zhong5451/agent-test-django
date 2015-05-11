6d3b4e0fe3976f0aca1132d26dd43df361279961
zhongxw <zhongxw@qssec.com>
2015-05-11 16:01:56 +0800

    UPDATE: update alipay

--- pay/forms.py
+++ pay/forms.py
@@ -11,9 +11,10 @@ from django import forms
 class PaymentForm(forms.Form):
     total_fee = forms.FloatField(min_value=0.01, max_value=100000000.00)
     domain_buy = forms.CharField(min_length=3, max_length=1280)
-    service_id = forms.IntegerField(min_value=1)
-    flag = forms.IntegerField(min_value=0, max_value=1)
-    sign = forms.CharField()
-
-    def __init__(self, *args, **kwargs):
-        super(PaymentForm, self).__init__(*args, **kwargs)
+    service = forms.IntegerField(min_value=1)
+    # flag = forms.IntegerField(min_value=0, max_value=1)
+    user_id = forms.IntegerField(min_value=0, max_value=1)
+    out_trade_no = forms.CharField(max_length=32)
+    subject = forms.CharField(max_length=64)
+    body = forms.CharField(max_length=128)
+    # sign = forms.CharField()
--- pay/views.py
+++ pay/views.py
@@ -7,12 +7,10 @@ Created on 2015-05-05
 
 from alipay.create_direct_pay_by_user.forms import AliPayDirectPayForm
 from alipay.helpers import make_sign, get_form_data
-from decimal import Decimal
 from django.conf import settings
 from pay.forms import PaymentForm
 from utils.helper import json_response
 import urllib2
-import uuid
 
 
 def pay_by_alipay(request):
@@ -22,74 +20,47 @@ def pay_by_alipay(request):
     if not form.is_valid():
         return json_response({'status': 400})
 
-    total_fee_form = form.cleaned_data.get('total_fee', 0.01)
+    total_fee = form.cleaned_data.get('total_fee', 0.01)
     domain_buy = form.cleaned_data.get('domain_buy', '')
-
-    # service_info = Service_rank_info.objects.all()
-    # service_dict = {service.service_id: service for service in service_info}
-    # sum total fee is correct?
-    pay_url = settings.DOMAIN
-    out_trade_no = uuid.uuid1().hex
-    total_payments = Decimal(0)
-    description_goods = u''
-
-    # source_service = service_dict.get(item.service_version_id, {})
-    # if not source_service:
-    #     return qs_conf.RETURN_SERVICE_ERROR
-    description_goods = u'为域名【%s】购买套餐【%s】' % (domain_buy,
-                        'source_service.service_name')
-    try:
-        unit_price = int(100)
-        quantity = int(3)
-    except ValueError:
-        return json_response({'status': 400})
-
-    subject = u"青松抗D防御套餐"
-    detail_body = description_goods
-
-    # orders = Order_form.objects.filter(out_trade_no=out_trade_no, user=uid)
-    # if not orders:
-    #     order = Order_form.objects.create(out_trade_no = out_trade_no,
-    #                                       user_id = uid,
-    #                                       payment_money = Decimal(str(total_payments)),
-    #                                       payment_way = qs_conf.order_form_payment_way[0],
-    #                                       desc = detail_body,
-    #                                       )
-    #     order.save()
-
-    # orders.update(payment_money=Decimal(str(total_payments)), desc = detail_body)
+    service = form.cleaned_data.get('service', '')
+    user_id = form.cleaned_data.get('user_id', '')
+    out_trade_no = form.cleaned_data.get('out_trade_no', '')
+    subject = form.cleaned_data.get('subject', '')
+    body = form.cleaned_data.get('body', '')
 
     # alipay form
     alipay_dict = {
             "_input_charset": 'utf-8',
-            'notify_url': '%s/alipay/return_async/' % str(pay_url),
-            #'return_url': '%s/api/alipay/return/' % pay_url,
+            'notify_url': '%s/alipay/nofify_async/' % settings.DOMAIN,
+            # 'return_url': '%s/api/alipay/return/' % settings.DOMAIN,
             'sign_type': 'MD5',
-            #'sign': '',
-            #'error_notify_url': '',
+            # 'sign': '',
+            # 'error_notify_url': '',
             'out_trade_no': out_trade_no,
             'subject': subject,
-            #'buyer_id': '',
-            #'seller_account_name': '',
-            #'buyer_account_name': '',
+            # 'buyer_id': '',
+            # 'seller_account_name': '',
+            # 'buyer_account_name': '',
             'seller_email': settings.ALIPAY_SELLER_EMAIL,
-            #'price': '',
-            'total_fee': 220,
-            #'quantity': '',
-            #'it_b_pay': '1h',   # 超时时间m-分钟,h-小时,d-天,1c-当天(无论交易何时创建,都在 0 点关闭)。 该参数数值不接受小数点, 如1.5h,可转换为 90m
-            'body': detail_body,
-            #'show_url': '',
-            #'discount': '',
-            #'need_ctu_check': 'Y',
-            #'royalty_type': '',
-            #'royalty_parameters': '',
+            # 'price': '',
+            'total_fee': 0.01,
+            # 'quantity': '',
+            # 超时时间m-分钟,h-小时,d-天,1c-当天(无论交易何时创建,都在0点关闭)。
+            # 该参数数值不接受小数点, 如1.5h,可转换为 90m
+            # 'it_b_pay': '1h',
+            'body': body,
+            # 'show_url': '',
+            # 'discount': '',
+            # 'need_ctu_check': 'Y',
+            # 'royalty_type': '',
+            # 'royalty_parameters': '',
             'anti_phishing_key': 'AABBCDDEG',
-            #'exter_invoke_ip': '',
+            # 'exter_invoke_ip': '',
             'extra_common_param': 'This is a test request',
-            #'extend_param': '',
+            # 'extend_param': '',
             'default_login': 'Y',
-            #'product_type': '',
-            #'token': '',
+            # 'product_type': '',
+            # 'token': '',
             }
     alipay_form = AliPayDirectPayForm(auto_id=False, initial=alipay_dict)
     data = get_form_data(alipay_form)
@@ -106,7 +77,6 @@ def pay_by_alipay(request):
         except AttributeError:
             pass
         request_url += "&%s=%s" % (key, v)
-    # return HttpResponseRedirect(request_url)
     return_data = {}
     return_data['alipay_url'] = request_url
     return_data.update()
