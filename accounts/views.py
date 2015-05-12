65bdbef137282b3d384bd08a3a60056e8230e0eb
zhongxw <zhongxw@qssec.com>
2015-05-12 11:31:57 +0800

    UPDATE: update render add cookie use r.json()

--- accounts/views.py
+++ accounts/views.py
@@ -59,6 +59,7 @@ def home(request):
     r = requests.post(url, data=data)
     print client_ip
     print r.text
+    res = r.json()
     response = render(request, 'home.html', {})
-    response.set_cookie(key='pk', value=r.text)
+    response.set_cookie(key='pk', value=res.get('pk', ''))
     return response
