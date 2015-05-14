57d9b094832d051fd4246e340d943941d608ffd8
zhongxw <zhongxw@qssec.com>
2015-05-14 14:15:18 +0800

    FIX: NameError cncrypt

--- pay/views.py
+++ pay/views.py
@@ -10,7 +10,7 @@ from alipay.helpers import make_sign, get_form_data
 from django.conf import settings
 from django.shortcuts import render
 from pay.forms import PaymentForm
-from utils.helper import json_response, decrypt , Param
+from utils.helper import json_response, decrypt , Param, encrypt
 import hashlib
 import requests
 import urllib2
